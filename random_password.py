d8be0bbd3ee4f673698218cd45c8a52bb90079f2
Karol <43761580+koncewiczkarol@users.noreply.github.com>
2018-10-05 08:32:17 +0200

    Update and rename practicepython_ex16_v02.py to random_password.py

--- random_password.py
+++ random_password.py
@@ -1,14 +1,14 @@
-dlugosc = int(input("Wprowadź długość hasła (ilość znaków) do wygenerowania: "))
+dlugosc = int(input("How long (number of signs) password You need? "))
 
+# all of the signs used for making new password
 litery = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 cyfry = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 znaki = ["!", "@", "#", "$", "%"]
 
 haslo_final = str("")
-
 import random
 
-for i in range(dlugosc):
+for i in range(dlugosc): # loop that chooses randomly between letters, capital letters, numbers, symbols and makes a final password
     x = random.randrange(0,6)
     if x < 2:
         y = random.choice(litery)
@@ -24,6 +24,3 @@ for i in range(dlugosc):
         haslo_final += y
 
 print(haslo_final)
-
-
-input("\n\n\t\t\t \\ Aby zakończyć \n\t\t\tnaciśnij 'enter' \\")
